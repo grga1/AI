33c8544b670affbec6a80ec7060b25dc2422cd9b
Filip Iliev <151552696+grga1@users.noreply.github.com>
2025-09-04 03:27:21 +0200

    Update 1.py

--- ISPIT_SEPTEMVRI_POLOZUVAM/Подготовка за втори колоквиуми/1.py
+++ ISPIT_SEPTEMVRI_POLOZUVAM/Подготовка за втори колоквиуми/1.py
@@ -2,43 +2,47 @@ import os
 
 os.environ['OPENBLAS_NUM_THREADS'] = '1'
 from dataset_script import dataset
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.naive_bayes import GaussianNB
 import math
+from sklearn.naive_bayes import  GaussianNB
+from  sklearn.preprocessing import MinMaxScaler
+
+
 if __name__ == '__main__':
     C = int(input())
     P = int(input())
+    P=P/100
 
     new_dataset = []
     for row in dataset:
         first = float(row[0])+float(row[-2])
         new_dataset.append([first]+row[1:-2]+[row[-1]])
 
-    dataset = new_dataset
-    good = [ row for row in dataset if row[-1]=='good']
-    bad = [ row for row in dataset if row[-1]=='bad']
+    dataset=new_dataset
 
-    P=P/100
+    good = [row for row in dataset if row[-1]=='good']
+    bad = [row for row in dataset if row[-1]=='bad']
+
     if C==0:
         train_data = good[:int(P*len(good))]+bad[:int(P*len(bad))]
         test_data = good[int(P*len(good)):]+bad[int(P*len(bad)):]
     elif C==1:
-        train_data = good[:-int(P * len(good))] + bad[:-int(P * len(bad))]
-        test_data = good[-int(P * len(good)):] + bad[-int(P * len(bad)):]
-
-    test_x = [row[:-1] for row in test_data]
-    test_y = [row[-1] for row in test_data]
+        train_data = good[-int(math.ceil(P * len(good))):] + bad[-int(math.ceil(P * len(bad))):]
+        test_data = good[:-int(math.ceil(P * len(good)))] + bad[:-int(math.ceil(P * len(bad)))]
 
     train_x = [row[:-1] for row in train_data]
     train_y = [row[-1] for row in train_data]
 
-    scaler = MinMaxScaler(feature_range=(-1,1))
+    test_x = [row[:-1] for row in test_data]
+    test_y = [row[-1] for row in test_data]
+
     classifier1 = GaussianNB()
-    classifier1.fit(train_x, train_y)
-    print(f'Tochnost so zbir na koloni: {classifier1.score(test_x, test_y)}')
+    classifier1.fit(train_x,train_y)
+    print(f'Tochnost so zbir na koloni: {classifier1.score(test_x,test_y)}')
     classifier2 = GaussianNB()
+    scaler = MinMaxScaler(feature_range=(-1,1))
     scaler.fit(train_x)
     train_x = scaler.transform(train_x)
     test_x = scaler.transform(test_x)
     classifier2.fit(train_x,train_y)
-    classifier2.score(test_x,train_y)
+    print(f'Tochnost so zbir na koloni i skaliranje: {classifier2.score(test_x,test_y)}')
+
